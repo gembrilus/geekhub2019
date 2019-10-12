ff3d4ae38f3b5aeb27bf78c3a6220475b69a1640
Максим Наконечный <gembrilus@gmail.com>
2019-10-12 11:13:59 +0300

    Almost ready structure of the programm

--- Aboutme/app/src/main/java/com/example/aboutme/Settings.kt
+++ Aboutme/app/src/main/java/com/example/aboutme/Settings.kt
@@ -5,6 +5,12 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_settings.*
+import kotlinx.android.synthetic.main.part_address_info.*
+import kotlinx.android.synthetic.main.part_interest.*
+import kotlinx.android.synthetic.main.part_job_and_study.*
+import kotlinx.android.synthetic.main.part_main_info.*
+import kotlinx.android.synthetic.main.part_photo_picker.*
+import kotlinx.android.synthetic.main.part_social_settings.*
 
 class Settings : AppCompatActivity() {
 
@@ -12,22 +18,34 @@ class Settings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        group1_minimize.setOnClickListener {
-            if(hided_group1.isVisible) hided_group1.visibility = View.GONE
-            else hided_group1.visibility = View.VISIBLE
-/*            if(ll_group1.getChildAt(1).isVisible) {
-                for (i in 1 until ll_group1.size) {
-                    val v = ll_group1.getChildAt(i)
-                    v.visibility = View.GONE
-                }
-            }
-            else{
-                for (i in 1 until ll_group1.size) {
-                    val v = ll_group1.getChildAt(i)
-                    v.visibility = View.VISIBLE
-                }
-            }*/
+        hided_group1.setOnClickListener {
+            if(ll_group1.isVisible) ll_group1.visibility = View.GONE
+            else ll_group1.visibility = View.VISIBLE
+        }
+
+        hided_group2.setOnClickListener {
+            if(ll_group2.isVisible) ll_group2.visibility = View.GONE
+            else ll_group2.visibility = View.VISIBLE
+        }
+
+        hided_group3.setOnClickListener {
+            if(ll_group3.isVisible) ll_group3.visibility = View.GONE
+            else ll_group3.visibility = View.VISIBLE
+        }
+
+        hided_group4.setOnClickListener {
+            if(ll_group4.isVisible) ll_group4.visibility = View.GONE
+            else ll_group4.visibility = View.VISIBLE
+        }
+
+        hided_group5.setOnClickListener {
+            if(ll_group5.isVisible) ll_group5.visibility = View.GONE
+            else ll_group5.visibility = View.VISIBLE
+        }
+
+        hided_group6.setOnClickListener {
+            if(ll_group6.isVisible) ll_group6.visibility = View.GONE
+            else ll_group6.visibility = View.VISIBLE
         }
     }
-
 }
