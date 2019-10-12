487ec2f2528d5c929d40cd3b6257299baca735cd
Максим Наконечный <gembrilus@gmail.com>
2019-10-12 21:16:41 +0300

    Added two layouts for work, study, hobbies

---
+++ Aboutme/app/src/main/java/com/example/aboutme/DatePickerFragment.kt
@@ -0,0 +1,33 @@
+package com.example.aboutme
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.part_main_info.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DatePickerFragment: DialogFragment(), DatePickerDialog.OnDateSetListener {
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+        activity?.tw_birthday?.text = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(calendar.time)
+    }
+
+    override fun onCancel(dialog: DialogInterface?) {
+        Toast.makeText(activity, "Дата рождения не была выбрана", Toast.LENGTH_SHORT).show()
+        super.onCancel(dialog)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(context!!,this, year, month, day)
+    }
+}
--- Aboutme/app/src/main/java/com/example/aboutme/Settings.kt
+++ Aboutme/app/src/main/java/com/example/aboutme/Settings.kt
@@ -2,8 +2,8 @@ package com.example.aboutme
 
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_settings.*
 import kotlinx.android.synthetic.main.part_address_info.*
 import kotlinx.android.synthetic.main.part_interest.*
@@ -12,40 +12,46 @@ import kotlinx.android.synthetic.main.part_main_info.*
 import kotlinx.android.synthetic.main.part_photo_picker.*
 import kotlinx.android.synthetic.main.part_social_settings.*
 
-class Settings : AppCompatActivity() {
+class Settings : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         hided_group1.setOnClickListener {
-            if(ll_group1.isVisible) ll_group1.visibility = View.GONE
+            if (ll_group1.isVisible) ll_group1.visibility = View.GONE
             else ll_group1.visibility = View.VISIBLE
         }
 
         hided_group2.setOnClickListener {
-            if(ll_group2.isVisible) ll_group2.visibility = View.GONE
+            if (ll_group2.isVisible) ll_group2.visibility = View.GONE
             else ll_group2.visibility = View.VISIBLE
         }
 
         hided_group3.setOnClickListener {
-            if(ll_group3.isVisible) ll_group3.visibility = View.GONE
+            if (ll_group3.isVisible) ll_group3.visibility = View.GONE
             else ll_group3.visibility = View.VISIBLE
         }
 
         hided_group4.setOnClickListener {
-            if(ll_group4.isVisible) ll_group4.visibility = View.GONE
+            if (ll_group4.isVisible) ll_group4.visibility = View.GONE
             else ll_group4.visibility = View.VISIBLE
         }
 
         hided_group5.setOnClickListener {
-            if(ll_group5.isVisible) ll_group5.visibility = View.GONE
+            if (ll_group5.isVisible) ll_group5.visibility = View.GONE
             else ll_group5.visibility = View.VISIBLE
         }
 
         hided_group6.setOnClickListener {
-            if(ll_group6.isVisible) ll_group6.visibility = View.GONE
+            if (ll_group6.isVisible) ll_group6.visibility = View.GONE
             else ll_group6.visibility = View.VISIBLE
         }
+
+        ib_date.setOnClickListener {
+            DatePickerFragment().show(supportFragmentManager, "Выбор даты рождения")
+        }
+
+
     }
 }
